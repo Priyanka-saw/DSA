a2a7e9671a4f8b36324258e6d97a368530cbd588
Priyanka-saw <priyankasaw51@gmail.com>
2025-06-09 22:28:34 +0530

    initial commit

---
+++ array/SecLarge.java
@@ -0,0 +1,23 @@
+public class SecLarge {
+    public static void main(String[] args){
+
+        int[] arr = {1, 3, 5, 6};
+        System.out.println("Second largest number" + " : " + seclarge(arr));
+    }
+
+    public static int seclarge(int[] arr){
+        int largest = -1 , seclargest = -1;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] > largest){
+                largest = arr[i];
+            }
+        }
+       
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] > seclargest && arr[i] != largest){
+                seclargest = arr[i];
+            }
+        }
+        return seclargest;
+    }
+}
--- practice.java
+++ practice.java
@@ -1,5 +1,17 @@
 public class practice {
     public static void main(String[] args) {
+
         int[] arr = {1, 3, 5, 7, 8};
+        System.out.println(maxVal(arr));
+    }
+    public static int maxVal(int[] arr){
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]> max){
+                max = arr[i];
+            }
+
+        }
+        return max;
     }
 }
